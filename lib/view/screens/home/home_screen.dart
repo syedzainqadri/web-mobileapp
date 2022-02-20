1cea6d33b4b948f82628b82d93bb10b11e971c79
EwigLife <zain@ewiglife.com>
2022-02-20 14:55:37 +0500

    handing over to niaz

--- lib/view/screens/home/home_screen.dart
+++ lib/view/screens/home/home_screen.dart
@@ -106,142 +106,6 @@ class HomeScreen extends StatelessWidget {
                                 ? SizedBox()
                                 : DailyItemView();
                       }),
-                      // Consumer<CategoryProvider>(
-                      //   builder: (context, categoryProvider, child) {
-                      //     return categoryProvider.categoryList.length != 0
-                      //         ? Row(children: [
-                      //             Container(
-                      //               width: 100,
-                      //               margin: EdgeInsets.only(top: 3),
-                      //               height: double.infinity,
-                      //               decoration: BoxDecoration(
-                      //                 //color: ColorResources.WHITE,
-                      //                 boxShadow: [
-                      //                   BoxShadow(
-                      //                       color: Colors.grey[
-                      //                           Provider.of<ThemeProvider>(
-                      //                                       context)
-                      //                                   .darkTheme
-                      //                               ? 600
-                      //                               : 200],
-                      //                       spreadRadius: 3,
-                      //                       blurRadius: 10)
-                      //                 ],
-                      //               ),
-                      //               child: ListView.builder(
-                      //                 physics: BouncingScrollPhysics(),
-                      //                 itemCount:
-                      //                     categoryProvider.categoryList.length,
-                      //                 padding: EdgeInsets.all(0),
-                      //                 itemBuilder: (context, index) {
-                      //                   CategoryModel _category =
-                      //                       categoryProvider
-                      //                           .categoryList[index];
-                      //                   return InkWell(
-                      //                     onTap: () {
-                      //                       categoryProvider
-                      //                           .changeSelectedIndex(index);
-                      //                       categoryProvider.getSubCategoryList(
-                      //                           context,
-                      //                           _category.id.toString(),
-                      //                           Provider.of<LocalizationProvider>(
-                      //                                   context,
-                      //                                   listen: false)
-                      //                               .locale
-                      //                               .languageCode);
-                      //                     },
-                      //                     child: CategoryItem(
-                      //                       title: _category.name,
-                      //                       icon: _category.image,
-                      //                       isSelected: categoryProvider
-                      //                               .categorySelectedIndex ==
-                      //                           index,
-                      //                     ),
-                      //                   );
-                      //                 },
-                      //               ),
-                      //             ),
-                      //             categoryProvider.subCategoryList != null
-                      //                 ? Expanded(
-                      //                     child: ListView.builder(
-                      //                       padding: EdgeInsets.all(
-                      //                           Dimensions.PADDING_SIZE_SMALL),
-                      //                       itemCount: categoryProvider
-                      //                               .subCategoryList.length +
-                      //                           1,
-                      //                       itemBuilder: (context, index) {
-                      //                         if (index == 0) {
-                      //                           return ListTile(
-                      //                             onTap: () {
-                      //                               Navigator.of(context)
-                      //                                   .pushNamed(
-                      //                                 RouteHelper
-                      //                                     .getCategoryProductsRoute(
-                      //                                   categoryProvider
-                      //                                       .categoryList[
-                      //                                           categoryProvider
-                      //                                               .categorySelectedIndex]
-                      //                                       .id,
-                      //                                 ),
-                      //                                 arguments:
-                      //                                     CategoryProductScreen(
-                      //                                         categoryModel:
-                      //                                             CategoryModel(
-                      //                                   id: categoryProvider
-                      //                                       .categoryList[
-                      //                                           categoryProvider
-                      //                                               .categorySelectedIndex]
-                      //                                       .id,
-                      //                                   name: categoryProvider
-                      //                                       .categoryList[
-                      //                                           categoryProvider
-                      //                                               .categorySelectedIndex]
-                      //                                       .name,
-                      //                                 )),
-                      //                               );
-                      //                             },
-                      //                             title: Text(getTranslated(
-                      //                                 'all', context)),
-                      //                             trailing: Icon(Icons
-                      //                                 .keyboard_arrow_right),
-                      //                           );
-                      //                         }
-                      //                         return ListTile(
-                      //                           onTap: () {
-                      //                             Navigator.of(context)
-                      //                                 .pushNamed(
-                      //                               RouteHelper
-                      //                                   .getCategoryProductsRoute(
-                      //                                       categoryProvider
-                      //                                           .subCategoryList[
-                      //                                               index - 1]
-                      //                                           .id),
-                      //                             );
-                      //                           },
-                      //                           title: Text(
-                      //                             categoryProvider
-                      //                                 .subCategoryList[
-                      //                                     index - 1]
-                      //                                 .name,
-                      //                             style: poppinsMedium.copyWith(
-                      //                                 fontSize: 13,
-                      //                                 color: ColorResources
-                      //                                     .getTextColor(
-                      //                                         context)),
-                      //                             overflow:
-                      //                                 TextOverflow.ellipsis,
-                      //                           ),
-                      //                           trailing: Icon(
-                      //                               Icons.keyboard_arrow_right),
-                      //                         );
-                      //                       },
-                      //                     ),
-                      //                   )
-                      //                 : Expanded(child: SubCategoryShimmer()),
-                      //           ])
-                      //         : Center(child: CircularProgressIndicator());
-                      //   },
-                      // ),
                       // Banner Two
                       Consumer<BannerTwoProvider>(
                           builder: (context, bannerTwo, child) {
@@ -258,27 +122,96 @@ class HomeScreen extends StatelessWidget {
                         child: Consumer<CategoryProvider>(
                           builder: (context, categoryProvider, child) {
                             return categoryProvider.categoryList.length != 0
-                                ? ListView.builder(
-                                    itemCount:
-                                        categoryProvider.categoryList.length,
-                                    padding: EdgeInsets.all(8.0),
-                                    itemBuilder: (context, index) {
-                                      CategoryModel _category =
-                                          categoryProvider.categoryList[index];
-                                      return Row(
-                                        children: [
-                                          Expanded(
-                                            child: Card(
-                                              child: Padding(
-                                                padding:
-                                                    const EdgeInsets.all(8.0),
-                                                child: Text(_category.name),
-                                              ),
+                                ? Row(
+                                    children: [
+                                      Container(
+                                        height: 800,
+                                        width: 800,
+                                        child: ListView.builder(
+                                            itemCount: categoryProvider
+                                                .categoryList.length,
+                                            padding: EdgeInsets.all(8.0),
+                                            itemBuilder: (context, index) {
+                                              CategoryModel _category =
+                                                  categoryProvider
+                                                      .categoryList[index];
+                                              return InkWell(
+                                                onTap: () {
+                                                  categoryProvider
+                                                      .changeSelectedIndex(
+                                                          index);
+                                                  categoryProvider
+                                                      .getSubCategoryList(
+                                                          context,
+                                                          _category.id
+                                                              .toString(),
+                                                          Provider.of<LocalizationProvider>(
+                                                                  context,
+                                                                  listen: false)
+                                                              .locale
+                                                              .languageCode);
+                                                },
+                                                child: CategoryItem(
+                                                  title: _category.name,
+                                                  icon: _category.image,
+                                                  isSelected: categoryProvider
+                                                          .categorySelectedIndex ==
+                                                      index,
+                                                ),
+                                              );
+                                            }),
+                                      ),
+                                      categoryProvider.subCategoryList != null
+                                          ? Expanded(
+                                              child: ListView.builder(
+                                                  padding: EdgeInsets.all(
+                                                      Dimensions
+                                                          .PADDING_SIZE_SMALL),
+                                                  itemCount: categoryProvider
+                                                      .subCategoryList.length,
+                                                  itemBuilder:
+                                                      (context, index) {
+                                                    return ListTile(
+                                                      onTap: () {
+                                                        Navigator.of(context)
+                                                            .pushNamed(
+                                                          RouteHelper
+                                                              .getCategoryProductsRoute(
+                                                            categoryProvider
+                                                                .categoryList[
+                                                                    categoryProvider
+                                                                        .categorySelectedIndex]
+                                                                .id,
+                                                          ),
+                                                          arguments:
+                                                              CategoryProductScreen(
+                                                                  categoryModel:
+                                                                      CategoryModel(
+                                                            id: categoryProvider
+                                                                .categoryList[
+                                                                    categoryProvider
+                                                                        .categorySelectedIndex]
+                                                                .id,
+                                                            name: categoryProvider
+                                                                .categoryList[
+                                                                    categoryProvider
+                                                                        .categorySelectedIndex]
+                                                                .name,
+                                                          )),
+                                                        );
+                                                      },
+                                                      title: Text(getTranslated(
+                                                          'all', context)),
+                                                      trailing: Icon(Icons
+                                                          .keyboard_arrow_right),
+                                                    );
+                                                  }),
+                                            )
+                                          : Container(
+                                              child: Text('No Data'),
                                             ),
-                                          ),
-                                        ],
-                                      );
-                                    })
+                                    ],
+                                  )
                                 : Container(
                                     child: Text('category View'),
                                   );
